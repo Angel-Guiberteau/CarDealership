a5a0199674815652589c8493b0695882846b89d2
Angel-Guiberteau <aguiberteaufranco.guadalupe@alumnado.fundacionloyola.net>
2025-02-19 14:26:21 +0100

    Unifinsh view and layout

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -6,13 +6,18 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         @include('components.css')
         <script src="https://cdn.jsdelivr.net/npm/sweetalert/dist/sweetalert.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
         <title>@yield('title')</title>
         <style>
             .container-custom {
                 width: 97%;
-                background-color: #858585;
+                background-color: #adababb8;
                 margin: auto;
-                padding: 10px;
+                padding: 0.5%;
             }
         </style>
     </head>
@@ -22,5 +27,6 @@
         <div class="container-custom">
             @yield('content')
         </div>
+        
     </body>
 </html>
--- resources/views/adminpanel/cars.blade.php
+++ resources/views/adminpanel/cars.blade.php
@@ -21,7 +21,7 @@
             registros
         </div>
         <div>
-            <button class="btn btn-success">Agregar</button>
+            <button class="btn btn-dark">Agregar</button>
         </div>
     </div>
     
@@ -57,23 +57,71 @@
             </tr>
         </thead>
         <tbody>
-            @for ($i = 1; $i <= 10; $i++)
-                    <tr>
-                        <td>{{ $i }}</td>
-                        <td>Citroën C3</td>
-                        <td>Citroën</td>
-                        <td>SUV</td>
-                        <td>Gris</td>
-                        <td>60</td>
-                        <td>2002</td>
-                        <td>Sí</td>
-                        <td>Los modelos de gasolina son un 1.1L de 60 CV...</td>
-                        <td>2022-12-01-C3.jpg</td>
-                        <td><button class="btn btn-dark btn-sm">Editar</button></td>
-                        <td><button class="btn btn-danger btn-sm">Eliminar</button></td>
-                    </tr>
+            @for ($i = 1; $i <= 50; $i++)
+                <tr>
+                    <td>{{ $i }}</td>
+                    <td>Citroën C3</td>
+                    <td>Citroën</td>
+                    <td>SUV</td>
+                    <td>Gris</td>
+                    <td>60</td>
+                    <td>2002</td>
+                    <td>Sí</td>
+                    <td>Los modelos de gasolina son un 1.1L de 60 CV...</td>
+                    <td>2022-12-01-C3.jpg</td>
+                    <td><button class="btn btn-dark btn-sm">Editar</button></td>
+                    <td><button class="btn btn-danger btn-sm">Eliminar</button></td>
+                </tr>
             @endfor
         </tbody>
     </table>
+    
+    <div class="d-flex justify-content-between mt-3">
+        <div id="tableInfo"></div>
+        <div id="tablePagination" class="d-flex"></div>
+    </div>
 </div>
+
+<script>
+$(document).ready(function() {
+    var table = $('#vehiclesTable').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": true,
+        "ordering": true,
+        "info": true,  
+        "autoWidth": false,
+        "pageLength": 10,
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+            "infoEmpty": "No hay registros disponibles",
+            "infoFiltered": "(filtrado de _MAX_ registros en total)",
+            "search": "Buscar:",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        }
+    });
+
+    $('#tablePagination').append($('.dataTables_paginate'));
+    $('#tableInfo').append($('.dataTables_info'));
+
+    // Agregar filtrado por columna
+    $('#vehiclesTable thead .search-column').on('keyup change', function() {
+        let colIndex = $(this).parent().index();
+        table.column(colIndex).search(this.value).draw();
+    });
+
+    // Control de registros por página
+    $('#entries').on('change', function() {
+        var value = $(this).val();
+        table.page.len(value === "todos" ? -1 : parseInt(value)).draw();
+    });
+});
+</script>
 @endsection
