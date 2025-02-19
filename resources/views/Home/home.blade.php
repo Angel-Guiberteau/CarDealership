35917436dfb7ff53a4d422799df0eeb74a005343
zmartinl <zmartinllera@gmail.com>
2025-02-19 12:46:31 +0100

    Added Home Page

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Home')
 
 @section('filter')
-    <div class="container-fluid p-1 d-flex justify-content-around filter">
-        <div class="d-flex">
-            {{-- Brand --}}
+    <div class="container-fluid p-3 d-flex justify-content-around filter">
+        <div class="d-flex p-2">
+            <!-- Marca -->
             <div class="d-flex align-items-center me-5">
                 <label for="marca" class="form-label mt-2 me-4">Marca</label>
                 <select id="marca" class="form-select">
@@ -16,13 +16,13 @@
                 </select>
             </div>
 
-            {{-- Model --}}
+            <!-- Modelo -->
             <div class="d-flex align-items-center me-5">
                 <label for="modelo" class="form-label mt-2 me-4">Modelo</label>
                 <input type="text" id="modelo" class="form-control" placeholder="Ej. Corolla">
             </div>
 
-            {{-- Color --}}
+            <!-- Color -->
             <div class="d-flex align-items-center me-5">
                 <label for="color" class="form-label mt-2 me-4">Color</label>
                 <select id="color" class="form-select">
@@ -33,38 +33,40 @@
                 </select>
             </div>
 
-            {{-- Price (Dual Range Slider) --}}
+            <!-- Precio -->
             <div class="d-flex align-items-center me-5">
                 <label class="form-label mt-2 me-4">Precio</label>
                 <div class="range-container">
-                    <div class="d-flex mt-1 justify-content-end">
-                        <span id="precio_min_val" class="me-2">5000</span> - 
-                        <span id="precio_max_val" class="ms-2">100000</span>
+                    <!-- Contenedor de valores para alinearlos arriba y a la derecha -->
+                    <div class="range-values">
+                        <span id="precio_min_val">5000</span> - 
+                        <span id="precio_max_val">100000</span>
                     </div>
-                    <input type="range" id="precio_min" class="form-range" min="5000" max="100000" step="1000" value="5000">
-                    <input type="range" id="precio_max" class="form-range" min="5000" max="100000" step="1000" value="100000">
+                    <div class="slider-track"></div>
+                    <input type="range" id="precio_min" min="5000" max="100000" step="1000" value="5000">
+                    <input type="range" id="precio_max" min="5000" max="100000" step="1000" value="100000">
                 </div>
             </div>
 
-            {{-- HorsePower (Dual Range Slider) --}}
+            <!-- Potencia -->
             <div class="d-flex align-items-center me-5">
                 <label class="form-label mt-2 me-4">Potencia</label>
                 <div class="range-container">
-                    <div class="d-flex mt-1 justify-content-end">
-                        <span id="potencia_min_val" class="me-2">50</span> - 
-                        <span id="potencia_max_val" class="ms-2">1000</span>
+                    <!-- Contenedor de valores para alinearlos arriba y a la derecha -->
+                    <div class="range-values">
+                        <span id="potencia_min_val">50</span> - 
+                        <span id="potencia_max_val">1000</span>
                     </div>
-                    <input type="range" id="potencia_min" class="form-range" min="50" max="1000" step="10" value="50">
-                    <input type="range" id="potencia_max" class="form-range" min="50" max="1000" step="10" value="1000">
+                    <div class="slider-track"></div>
+                    <input type="range" id="potencia_min" min="50" max="1000" step="10" value="50">
+                    <input type="range" id="potencia_max" min="50" max="1000" step="10" value="1000">
                 </div>
             </div>
         </div>
 
-        <div class="d-flex justify-content-end">
-            {{-- Reset Button --}}
-            <div class="d-flex align-items-center">
-                <button class="btn resetFilter">Restablecer</button>
-            </div>
+        <!-- Botón Restablecer -->
+        <div class="d-flex">
+            <button id="reset" class="btn resetFilter mt-2">Restablecer</button>
         </div>
     </div>
 @endsection
