056fd6738dac37e0417ef751a765a953a086aa17
Angel-Guiberteau <aguiberteaufranco.guadalupe@alumnado.fundacionloyola.net>
2025-02-19 14:47:34 +0100

    Created & finished admin js component

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -5,10 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         @include('components.admin_css')
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert/dist/sweetalert.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+        @include('components.admin_js')
         <title>@yield('title')</title>
     </head>
     <body>
