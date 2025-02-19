667a69e2cdfa07c7977aab1d8661863d2988c300
Angel-Guiberteau <aguiberteaufranco.guadalupe@alumnado.fundacionloyola.net>
2025-02-19 14:42:52 +0100

    Finished admind css component

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -6,20 +6,10 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         @include('components.admin_css')
         <script src="https://cdn.jsdelivr.net/npm/sweetalert/dist/sweetalert.min.js"></script>
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
         <title>@yield('title')</title>
-        <style>
-            .container-custom {
-                width: 97%;
-                background-color: #adababb8;
-                margin: auto;
-                padding: 0.5%;
-            }
-        </style>
     </head>
     <body>
         @include('components.header')
