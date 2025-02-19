98682625eacf36deb5e1fe4fb60abf064eda8ca5
Angel-Guiberteau <aguiberteaufranco.guadalupe@alumnado.fundacionloyola.net>
2025-02-19 12:17:09 +0100

    Edited adminlayout container

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -7,11 +7,19 @@
         @include('components.css')
         <script src="https://cdn.jsdelivr.net/npm/sweetalert/dist/sweetalert.min.js"></script>
         <title>@yield('title')</title>
+        <style>
+            .container-custom {
+                width: 97%;
+                background-color: #858585;
+                margin: auto;
+                padding: 10px;
+            }
+        </style>
     </head>
     <body>
         @include('components.header')
         @include('components.admin_nav')
-        <div class="container">
+        <div class="container-custom">
             @yield('content')
         </div>
     </body>
