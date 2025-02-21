0420a4c5d3c69517f01d45da5b14fd84b1425d6e
JoaquinFTN <joaquinfranciscotelonunez.guadalupe@alumnado.fundacionloyola.net>
2025-02-21 15:11:31 +0100

    Finished add types

--- resources/views/components/modals/types/modalAdd.blade.php
+++ resources/views/components/modals/types/modalAdd.blade.php
@@ -5,16 +5,17 @@
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Añadir nuevo tipo</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <form>
-                    <div class="mb-3 mt-3">
-                        <input type="text" class="form-control" id="brand" placeholder="Nombre *">
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary w-100">Agregar</button>
-            </div>
+            <form action="{{ route('addType')}}" method="post" accept-charset="UTF-8" >
+                @csrf
+                <div class="modal-body">
+                        <div class="mb-3 mt-3">
+                            <input type="text" class="form-control" name='name' id="type" placeholder="Nombre *">
+                        </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-secondary w-100">Agregar</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
