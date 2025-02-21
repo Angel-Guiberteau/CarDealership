c71809310e88ab2dd8dce7f28a785ff054f15b7a
JoaquinFTN <joaquinfranciscotelonunez.guadalupe@alumnado.fundacionloyola.net>
2025-02-21 10:09:43 +0100

    Soft changes

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use App\Models\Type;
 
 class TypeController extends Controller
 {
-    //
+    public static function index():View {
+        return view('adminpanel.Type')
+                ->with('types', Type::allTypes());
+    }
 }
